21d5ccfde42f07a0aa72fb13b7b955c1b1d80571
Yun Ding <yunding.eric@gmail.com>
2023-12-17 22:55:40 -0800

    enum instead of generics

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,51 @@
+#[derive(Debug)]
+enum DType {
+    F32(f32),
+    U8(u8),
+}
 /// Holds the math data, derivative, operation, as well as some metadata, such as the label
 #[derive(Debug)]
-struct Value<T> {
-    data: T
+struct Value {
+    data: DType
 }
 
-impl<T> Value<T> {
-    pub fn new(data: T) -> Self {
-        Value{data}
+impl From<f32> for DType {
+    fn from(value: f32) -> Self {
+        DType::F32(value)
+    }
+}
+
+impl From<u8> for DType {
+    fn from(value: u8) -> Self {
+        DType::U8(value)
+    }
+}
+
+impl std::ops::Add for DType {
+    type Output = DType;
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (DType::U8(v1), DType::F32(v2)) => (v1 as f32 + v2).into(),
+            (DType::U8(v1), DType::U8(v2)) => (v1 + v2).into(),
+            (DType::F32(v1), DType::F32(v2)) => (v1  + v2).into(),
+            (DType::F32(v1), DType::U8(v2)) => (v1  + v2 as f32).into(),
+
+        }
+    }
+}
+
+impl Value {
+    pub fn new(data: impl Into<DType>) -> Self {
+        Value { data: data.into() }
     }
 }
 
 /// Add operation
-impl<T> std::ops::Add for Value<T> {
-    type Output = Value<T>;
+impl std::ops::Add for Value {
+    type Output = Value;
     fn add(self, rhs: Self) -> Self::Output {
-        todo!()
+        let d = self.data + rhs.data;
+        Value::new(d)
     }
 }
 
@@ -25,9 +56,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        println!("{:?}", Value::new(1));
         let v1 = Value::new(1);
         let v2 = Value::new(1.0);
+        println!("{:?} {:?}", v1, v2);
         let v3 = v1 + v2;
+        println!("{:?}", v3);
     }
 }
