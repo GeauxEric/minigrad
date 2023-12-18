2daa62ed2be0c8d704b577dba5bdbda4dd014392
Yun Ding <yunding.eric@gmail.com>
2023-12-17 21:27:21 -0800

    naive generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,33 @@
+/// Holds the math data, derivative, operation, as well as some metadata, such as the label
+#[derive(Debug)]
+struct Value<T> {
+    data: T
+}
+
+impl<T> Value<T> {
+    pub fn new(data: T) -> Self {
+        Value{data}
+    }
+}
+
+/// Add operation
+impl<T> std::ops::Add for Value<T> {
+    type Output = Value<T>;
+    fn add(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::Value;
 
     #[test]
     fn it_works() {
-        assert_eq!(1, 1);
+        println!("{:?}", Value::new(1));
+        let v1 = Value::new(1);
+        let v2 = Value::new(1.0);
+        let v3 = v1 + v2;
     }
 }
