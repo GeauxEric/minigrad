f4a70e43daed8f0526accc5d0f80511a89a01ede
Yun Ding <yunding.eric@gmail.com>
2023-12-19 22:15:21 -0800

    extra fields

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum DType {
     F32(f32),
     U8(u8),
-}
-/// Holds the math data, derivative, operation, as well as some metadata, such as the label
-#[derive(Debug)]
-struct Value {
-    data: DType
 }
 
 impl From<f32> for DType {
@@ -27,9 +22,8 @@ impl std::ops::Add for DType {
         match (self, rhs) {
             (DType::U8(v1), DType::F32(v2)) => (v1 as f32 + v2).into(),
             (DType::U8(v1), DType::U8(v2)) => (v1 + v2).into(),
-            (DType::F32(v1), DType::F32(v2)) => (v1  + v2).into(),
-            (DType::F32(v1), DType::U8(v2)) => (v1  + v2 as f32).into(),
-
+            (DType::F32(v1), DType::F32(v2)) => (v1 + v2).into(),
+            (DType::F32(v1), DType::U8(v2)) => (v1 + v2 as f32).into(),
         }
     }
 }
@@ -46,9 +40,42 @@ impl std::ops::Mul for DType {
     }
 }
 
+#[derive(Debug, Clone)]
+enum Op {
+    NoOp,
+    Plus,
+    Mul,
+}
+
+/// Holds the math data, derivative, operation, as well as some metadata, such as the label
+#[derive(Debug, Clone)]
+struct Value {
+    /// current data
+    data: DType,
+
+    /// uid
+    label: String,
+
+    /// math operation that produces the data
+    op: Op,
+
+    /// values that were used to produce this value
+    /// TODO: avoid clone
+    prev: Vec<Value>,
+}
+
 impl Value {
     pub fn new(data: impl Into<DType>) -> Self {
-        Value { data: data.into() }
+        Value {
+            data: data.into(),
+            label: "".into(),
+            op: Op::NoOp,
+            prev: vec![],
+        }
+    }
+
+    pub fn set_label(&mut self, label: impl Into<String>) {
+        self.label = label.into();
     }
 }
 
@@ -56,8 +83,12 @@ impl Value {
 impl std::ops::Add for Value {
     type Output = Value;
     fn add(self, rhs: Self) -> Self::Output {
-        let d = self.data + rhs.data;
-        Value::new(d)
+        let d = self.clone().data + rhs.clone().data;
+        let mut v = Value::new(d);
+        v.op = Op::Plus;
+        v.prev.push(self);
+        v.prev.push(rhs);
+        v
     }
 }
 
@@ -65,11 +96,14 @@ impl std::ops::Add for Value {
 impl std::ops::Mul for Value {
     type Output = Value;
     fn mul(self, rhs: Self) -> Self::Output {
-        let d = self.data * rhs.data;
-        Value::new(d)
+        let d = self.clone().data * rhs.clone().data;
+        let mut v = Value::new(d);
+        v.op = Op::Mul;
+        v.prev.push(self);
+        v.prev.push(rhs);
+        v
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -77,13 +111,18 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let v1 = Value::new(1);
-        let v2 = Value::new(1.0);
+        let mut v1 = Value::new(1);
+        v1.set_label("v1");
+        let mut v2 = Value::new(1.0);
+        v2.set_label("v2");
         println!("{:?} {:?}", v1, v2);
-        let v3 = v1 + v2;
+        let mut v3 = v1 + v2;
+        v3.set_label("v3");
         println!("{:?}", v3);
-        let v4 = Value::new(3);
-        let v5 = v4 * v3;
+        let mut v4 = Value::new(3);
+        v4.set_label("v4");
+        let mut v5 = v4 * v3;
+        v5.set_label("v5");
         println!("{:?}", v5);
     }
 }
