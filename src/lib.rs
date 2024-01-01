25475c893d5311213e9903eff428aa3b1287578d
Yun Ding <yunding.eric@gmail.com>
2023-12-31 16:41:44 -0800

    Simplify by using f32 as the only data type

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,15 @@
 use std::cell::RefCell;
-use std::fmt::Formatter;
+use std::fmt::{Debug, Formatter};
 use std::ops;
 use std::rc::Rc;
 
-#[derive(Debug, Clone)]
-enum DataType {
-    F32(f32),
-}
-
-impl std::fmt::Display for DataType {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DataType::F32(v) => v.fmt(f),
-        }
-    }
-}
-
-impl ops::Add for DataType {
-    type Output = DataType;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        match (self, rhs) {
-            (DataType::F32(v1), DataType::F32(v2)) => DataType::F32(v1 + v2),
-        }
-    }
-}
-
-impl ops::Mul<f32> for &DataType {
-    type Output = f32;
-    fn mul(self, rhs: f32) -> Self::Output {
-        match self {
-            DataType::F32(f) => rhs * f,
-        }
-    }
-}
-
-impl ops::Mul for DataType {
-    type Output = DataType;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        match (self, rhs) {
-            (DataType::F32(v1), DataType::F32(v2)) => DataType::F32(v1 * v2),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Copy)]
+#[derive(Debug, Clone, PartialEq, Copy, Default)]
 enum Op {
-    NoOp,
+    #[default]
+    None,
     Plus,
     Mul,
-}
-
-impl Default for Op {
-    fn default() -> Self {
-        Op::NoOp
-    }
+    Tanh,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -72,13 +26,13 @@ impl ValueId {
 
 impl std::fmt::Display for ValueId {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
+        std::fmt::Display::fmt(&self.0, f)
     }
 }
 
 #[derive(Debug, Clone)]
 struct Value_ {
-    data: DataType,
+    data: f32,
     prev: Vec<Value>,
     op: Op,
     id: ValueId,
@@ -88,7 +42,7 @@ struct Value_ {
 impl std::fmt::Display for Op {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Op::NoOp => {
+            Op::None => {
                 write!(f, "{}", "no-op")
             }
             Op::Plus => {
@@ -96,6 +50,9 @@ impl std::fmt::Display for Op {
             }
             Op::Mul => {
                 write!(f, "{}", "*")
+            }
+            Op::Tanh => {
+                write!(f, "{}", "tanh")
             }
         }
     }
@@ -111,14 +68,21 @@ impl Clone for Value {
     }
 }
 
+fn tanh(value: &Value) -> Value {
+    let d = value.0.borrow().data.tanh();
+    let mut v = Value_::new(d);
+    v.with_op(Op::Tanh).with_child(value.clone());
+    Value(Rc::new(RefCell::new(v)))
+}
+
 impl Value_ {
-    pub fn new(data: DataType) -> Self {
+    pub fn new(data: f32) -> Self {
         Value_ {
             data,
             prev: vec![],
             op: Op::default(),
             id: ValueId::new(),
-            grad: 0f32,
+            grad: 0.0,
         }
     }
 
@@ -138,11 +102,11 @@ impl Value_ {
     }
 
     pub fn is_leaf(&self) -> bool {
-        return self.op == Op::NoOp;
+        return self.op == Op::None;
     }
 }
 
-fn get_siblings_data(children: &[Value], child: &Value) -> Vec<DataType> {
+fn get_siblings_data(children: &[Value], child: &Value) -> Vec<f32> {
     let mut data = vec![];
     for c in children {
         if c.0.borrow().id != child.0.borrow().id {
@@ -152,9 +116,9 @@ fn get_siblings_data(children: &[Value], child: &Value) -> Vec<DataType> {
     data
 }
 
-fn backprop(value: Value, siblings_data: Vec<DataType>, parent_op: Op, parent_grad: f32) {
+fn backprop(value: Value, siblings_data: Vec<f32>, parent_op: Op, parent_grad: f32) {
     match parent_op {
-        Op::NoOp => {
+        Op::None => {
             panic!("Should not reach here because parent value is already a leaf.")
         }
         Op::Plus => {
@@ -191,6 +155,22 @@ fn backprop(value: Value, siblings_data: Vec<DataType>, parent_op: Op, parent_gr
             for child_v in &children {
                 let my_siblings_data = get_siblings_data(&children, child_v);
                 backprop(child_v.clone(), my_siblings_data, v.op, v.grad)
+            }
+        }
+        Op::Tanh => {
+            // parent = tanh(value)
+            // d(parent) / d(value) = 1 - tanh(value) ** 2
+            let mut v = value.0.borrow_mut();
+            let d = v.data;
+            let grad = 1.0 - d.tanh() * d.tanh();
+            v.grad = grad * parent_grad;
+            if v.is_leaf() {
+                return;
+            }
+            let children = v.prev.clone();
+            for child_v in &children {
+                let my_siblings = get_siblings_data(&children, child_v);
+                backprop(child_v.clone(), my_siblings, v.op, v.grad)
             }
         }
     }
@@ -269,18 +249,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let v1 = Value(Rc::new(RefCell::new(Value_::new(DataType::F32(1.0)))));
-        let v2 = Value_::new(DataType::F32(2.0));
+        let v1 = Value(Rc::new(RefCell::new(Value_::new(0.1))));
+        let v2 = Value_::new(0.2);
         let v2 = Value(Rc::new(RefCell::new(v2)));
         let v3 = v1 + v2;
-        let v4 = Value_::new(DataType::F32(6.0));
+        let v4 = Value_::new(0.5);
         let v4 = Value(Rc::new(RefCell::new(v4)));
         let v5 = v3 * v4;
+        let v6 = tanh(&v5);
 
-        backprop_root(v5.clone());
+        backprop_root(v6.clone());
 
         let mut g = graph!(id!("computation"));
-        plot_computation_graph(&v5, &mut g);
+        plot_computation_graph(&v6, &mut g);
         let _graph_svg = exec(
             g,
             &mut PrinterContext::default(),
